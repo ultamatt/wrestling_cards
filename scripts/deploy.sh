52defee884702b876142bba738810fed6532263d
Matt Moeller <ultamatt@gmail.com>
2022-08-12 11:20:32 -0700

    Getting the paths right

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 pwd=$(pwd)
 
-
-cd /home/wrestling_cards/current/backend/frontend
+cd /home/wrestling_cards/current/frontend
 npm install
 npm run build
 
-cd /home/wrestling_cards/current/backend/backend
+cd /home/wrestling_cards/current/backend
 npm install
